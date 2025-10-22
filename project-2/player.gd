abd443107e20648e5e2c55276a9058b34240fc52
njahangir23 <njahangir@fordham.edu>
2025-10-22 13:02:22 -0400

    add character movement

--- project-2/player.gd
+++ project-2/player.gd
@@ -1,36 +1,38 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jumping = false
 
-@onready var animated_sprite_2d = get_node("AnimatedSprite2D")
-
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Apply gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
+	else:
+		# Reset jump state when on the ground
+		jumping = false
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jumping = true
+		animated_sprite_2d.play("jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	# Movement
+	var direction = Input.get_axis("move_left", "move_right")
+
+	if direction != 0:
 		velocity.x = direction * SPEED
-		if not jumping:
+		if not jumping and is_on_floor():
 			animated_sprite_2d.play("run")
-		if direction == -1:
-			animated_sprite_2d.flip_h = true
-		else:
-			animated_sprite_2d.flip_h = false
+		animated_sprite_2d.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if not jumping:
+		if not jumping and is_on_floor():
 			animated_sprite_2d.play("idle")
 
 	move_and_slide()
