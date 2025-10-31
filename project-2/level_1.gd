61c556ddd1b8322137db95065334c654ff2c4002
Lgazio <lcoutinhogazio@fordham.edu>
2025-10-31 18:21:23 -0400

    Update level_1.gd

--- project-2/level_1.gd
+++ project-2/level_1.gd
@@ -1,2 +1,6 @@
 extends Node2D
 @export var level_number = 1
+func _on_body_entered(body: Node2D) -> void:
+	# Only let the player go through if they collected the pickup
+	if body.is_in_group("player") and body.has_pickup:
+		get_tree().change_scene_to_file("res://level_2.tscn")
