f00d61e4b753773e685641c36e2842b48e84eee0
Lgazio <lcoutinhogazio@fordham.edu>
2025-10-31 18:30:26 -0400

    working

--- project-2/level_1.gd
+++ project-2/level_1.gd
@@ -1,5 +1,5 @@
 extends Node2D
-@export var level_number = 1
+
 func _on_body_entered(body: Node2D) -> void:
 	# Only let the player go through if they collected the pickup
 	if body.is_in_group("player") and body.has_pickup:
